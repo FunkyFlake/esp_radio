7abafb6c12dc7106dda1e212598faf639fc367f9
Jan Lauble <jan@lauble.eu>
2023-04-19 22:29:13 +0200

    Added wait4DREQ in read/write_reg function

--- src/vs1053.cpp
+++ src/vs1053.cpp
@@ -9,7 +9,7 @@ void VS1053::init() {
     pinMode(XDCS_PIN, OUTPUT);
     pinMode(DREQ_PIN, INPUT);
     
-    //Cycle control and data chip select pins (active low)
+    //Cycle control and data chip select pins (active low) to make intial state deterministic
     Serial.println("Initializing VS1053");
     digitalWrite(XCS_PIN, HIGH);
     digitalWrite(XDCS_PIN, HIGH);
@@ -34,6 +34,7 @@ void VS1053::init() {
     testSPI();
     SPI.endTransaction();
 
+    return;
 }
 
 void VS1053::write_reg(const uint8_t& reg, const uint16_t& data) const {
@@ -45,12 +46,15 @@ void VS1053::write_reg(const uint8_t& reg, const uint16_t& data) const {
         SPI.write(reg);
         SPI.write16(data);
         
+        wait4DREQ();        
+        
         digitalWrite(XCS_PIN, HIGH);
     }
     else
     {
         Serial.println("VS1053 is not ready for new data.");
     }
+    return;
 }
 
 uint16_t VS1053::read_reg(const uint8_t& reg) const {
@@ -62,7 +66,9 @@ uint16_t VS1053::read_reg(const uint8_t& reg) const {
     SPI.write(READ_CMD);
     SPI.write(reg);
     uint16_t data = SPI.transfer16(0xFFFF);
-    
+
+    wait4DREQ();
+
     digitalWrite(XCS_PIN, HIGH);
     
     return data;
@@ -70,10 +76,13 @@ uint16_t VS1053::read_reg(const uint8_t& reg) const {
 
 void VS1053::testSPI() const {
     Serial.println("Testing SPI:");
+ 
     Serial.println("Writing to register...");
     write_reg(REG_VOL, 0xCAFE);
+ 
     Serial.println("Reading from register...");
     uint16_t vol = read_reg(REG_VOL);
+    
     if(vol == 0xCAFE)
         Serial.println("SPI is functional.\n");
     else
@@ -86,4 +95,5 @@ void VS1053::testSPI() const {
 void VS1053::wait4DREQ() const {
     while(!digitalRead(DREQ_PIN))
         yield();
+    return;
 }
