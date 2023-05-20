89dcad91907a6efd683fe12db5e3de02aeaca2a8
Jan Lauble <jan@lauble.eu>
2023-05-20 20:58:52 +0200

    website in seprarate file

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 #include <mp3_example.h>
 #include <mp3_example2.h>
 #include <stdlib.h>
-//#include <website.h>
+#include <website.h>
 
 //Enter WiFi setup here:
 const char *SSID = "UPC230D88";
@@ -26,7 +26,6 @@ ESP8266WebServer server(80);
 
 String newStation = "0";
 String station = "0";
-String website = "<html>\n<head>\n  <title>Radio Station Selection</title>\n</head>\n<body>\n  <h1>Radio Station Selection</h1>\n\n  <form action=\"/\">\n    <label for=\"station\">Select a radio station:</label>\n    <select id=\"station\" name=\"station\">\n      <option value=\"0\">Radio Station 1</option>\n      <option value=\"1\">Radio Station 2</option>\n      <option value=\"2\">Radio Station 3</option>\n      <option value=\"3\">Radio Station 4</option>\n    </select>\n    <br>\n    <input type=\"submit\" value=\"Select\">\n  </form>\n</body>\n</html>";
 
 String station_urls[] = {"s3-webradio.rockantenne.de", "radiostreaming.ert.gr"};
 String station_paths[] = {"/rockantenne/stream/mp3", "/ert-kosmos"};
