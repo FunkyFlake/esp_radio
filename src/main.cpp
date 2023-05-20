d924903cce7b8467ae8e7c29f9b55177c3c7e60e
Jan Lauble <jan@lauble.eu>
2023-05-20 20:15:15 +0200

    Station switching is working

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <ESP8266mDNS.h>
 #include <mp3_example.h>
 #include <mp3_example2.h>
+#include <stdlib.h>
 //#include <website.h>
 
 //Enter WiFi setup here:
@@ -23,14 +24,25 @@ uint8_t mp3Buffer[32];
 
 ESP8266WebServer server(80);
 
-String station = "1";
-String website = "<html>\n<head>\n  <title>Radio Station Selection</title>\n</head>\n<body>\n  <h1>Radio Station Selection</h1>\n\n  <form action=\"/\">\n    <label for=\"station\">Select a radio station:</label>\n    <select id=\"station\" name=\"station\">\n      <option value=\"1\">Radio Station 1</option>\n      <option value=\"2\">Radio Station 2</option>\n      <option value=\"3\">Radio Station 3</option>\n      <option value=\"4\">Radio Station 4</option>\n    </select>\n    <br>\n    <input type=\"submit\" value=\"Select\">\n  </form>\n</body>\n</html>";
+String newStation = "0";
+String station = "0";
+String website = "<html>\n<head>\n  <title>Radio Station Selection</title>\n</head>\n<body>\n  <h1>Radio Station Selection</h1>\n\n  <form action=\"/\">\n    <label for=\"station\">Select a radio station:</label>\n    <select id=\"station\" name=\"station\">\n      <option value=\"0\">Radio Station 1</option>\n      <option value=\"1\">Radio Station 2</option>\n      <option value=\"2\">Radio Station 3</option>\n      <option value=\"3\">Radio Station 4</option>\n    </select>\n    <br>\n    <input type=\"submit\" value=\"Select\">\n  </form>\n</body>\n</html>";
+
+String station_urls[] = {"s3-webradio.rockantenne.de", "radiostreaming.ert.gr"};
+String station_paths[] = {"/rockantenne/stream/mp3", "/ert-kosmos"};
+  
+int status = 0;
+  
 
 void handleRoot() {
-  station = server.arg("station"); 
+  newStation = server.arg("station"); 
   Serial.print("Station:");
   Serial.println(station);
   server.send(200, "text/html", website);
+}
+
+int stationIdx(String &station) {
+  return atoi(station.c_str());
 }
 
 void setup() {
@@ -57,17 +69,12 @@ void setup() {
   server.begin();
   Serial.println("Web server started.");
 
-  String station_urls[] = {"s3-webradio.rockantenne.de", "radiostreaming.ert.gr"};
-
-  int status = 0;
   while(status != 1) {
-    status = client.connect(station_urls[0], 80);
-    //status = client.connect("radiostreaming.ert.gr", 80);
+    status = client.connect(station_urls[stationIdx(station)], 80);
   }
   Serial.println("Connected to radio station!");
   // Request stream
-  client.print("GET /rockantenne/stream/mp3 HTTP/1.1\r\nHost: s3-webradio.rockantenne.de\r\nConnection: close\r\n\r\n");
-  //client.print("GET /ert-kosmos HTTP/1.1\r\nHost: radiostreaming.ert.gr\r\nConnection: close\r\n\r\n");   
+  client.print("GET " + String(station_paths[stationIdx(station)]) + " HTTP/1.1\r\nHost: " + String(station_urls[stationIdx(station)]) + "\r\nConnection: close\r\n\r\n");
 }
 
 void loop() {
@@ -77,6 +84,17 @@ void loop() {
     MP3.playback(mp3Buffer, bytesread);
   }
   server.handleClient();
+
+  if(station != newStation) {
+    status = 0;
+    while(status != 1) {
+      status = client.connect(station_urls[stationIdx(newStation)], 80);
+    } 
+    Serial.println("Connected to radio station!");
+    // Request stream
+    client.print("GET " + String(station_paths[stationIdx(newStation)]) + " HTTP/1.1\r\nHost: " + String(station_urls[stationIdx(newStation)]) + "\r\nConnection: close\r\n\r\n");
+    station = newStation;
+  }
   //MP3.playback(mp3_data, sizeof(mp3_data));
   //delay(3000);
   //MP3.playback(mp3_data2, sizeof(mp3_data2));
